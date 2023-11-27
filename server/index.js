400690a1a002e4a46289ea31d1b556b1893831c6
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2023-11-27 19:14:16 +0530

    #commit5

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
+const cors = require('cors')
+app.use(cors())
 
 const myMiddleware = (req, res, next) => {
   next();
